af07287b05f581ddd9095dd4733fcf6c8805c74d
MattiaBiga200701 <mattia.biga@students.uniroma2.eu>
2024-09-09 17:50:19 +0200

    Dark Theme Implementation

--- app/src/main/java/com/example/trivia/ui/theme/Color.kt
+++ app/src/main/java/com/example/trivia/ui/theme/Color.kt
@@ -10,7 +10,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val MyBlack=Color(0xFF000000)
+val MyBlack=Color(0xFF3C3C3C)
 val MyGreen=Color(0xFF23FFCD)
 val MyPink=Color(0xFFF231AA)
 val MyRed=Color(0xFFDC143C)
